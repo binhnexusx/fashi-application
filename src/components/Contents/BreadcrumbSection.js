ad41dac81a157303768f635910f80cb430f61ec1
binhnexusx <binhhoangdev9@gmail.com>
2025-04-18 13:40:43 +0700

    complete split layouts

---
+++ src/components/Contents/BreadcrumbSection.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class BreadcrumbSection extends Component {
+    render() {
+        return (
+            <div>
+                <div classname="breacrumb-section">
+                    <div classname="container">
+                        <div classname="row">
+                            <div classname="col-lg-12">
+                                <div classname="breadcrumb-text">
+                                    <a href="#"><i classname="fa fa-home"> Home</i></a><i classname="fa fa-home">
+                                        <span>Shop</span>
+                                    </i></div><i classname="fa fa-home">
+                                </i></div><i classname="fa fa-home">
+                            </i></div><i classname="fa fa-home">
+                        </i></div><i classname="fa fa-home">
+                    </i></div><i classname="fa fa-home">
+                </i></div>
+        );
+    }
+}
+
+export default BreadcrumbSection;
